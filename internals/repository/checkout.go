7007b48d8ba2d17b0aa551b1de730c6ba10d494b
AMRITESH-BAKSHI <amritesh67bakshi12@gmail.com>
2026-05-29 00:49:18 +0530

    added the checkout feature with the recursive tree

--- internals/repository/checkout.go
+++ internals/repository/checkout.go
@@ -1,10 +1,11 @@
 package repository
 
-import ( 
-	"os"
+import (
 	"TimeScape/internals/objects"
 	"TimeScape/internals/storage"
-
+	
+	"os"
+	"path/filepath"
 )
 
 
@@ -18,30 +19,76 @@ func Checkout(commitHash string)error{
 	}
 
 	commit :=objects.ParseCommitData(commitData);
-	TreeContent ,err:=storage.ReadObject(commit.TreeHash);
+	// TreeContent ,err:=storage.ReadObject(commit.TreeHash);
+	err =RestoreTree(commit.TreeHash,".");
 	
 	if(err!=nil){
 		return err;
 	}
+	// entries :=objects.ParserTreeObject(TreeContent);
 
-	entries :=objects.ParserTreeObject(TreeContent);
-
-	for _,entry :=range entries {
-		blobdata,err :=storage.ReadObject(entry.Hash);
+	// for _,entry :=range entries {
+	// 	blobdata,err :=storage.ReadObject(entry.Hash);
 		
-	if(err!=nil){
-		return err;
-	}
-	content :=objects.ExtractBlobContent(blobdata);
+	
+	// content :=objects.ExtractBlobContent(blobdata);
 
-	err =os.WriteFile(entry.FileName,content,0644)
+	// err =os.WriteFile(entry.FileName,content,0644)
+	
+	// }
+	err =UpdateHead(commitHash);
+	
 	if(err!=nil){
 		return err;
 	}
 	
-}
-UpdateHead(commitHash);
+
 return nil;
 
 }
 
+
+
+func RestoreTree(treeHash string,currentPath string)error{
+
+		treeData, err :=storage.ReadObject(treeHash);
+		if(err!=nil){
+			return err;
+		}
+
+		entries :=objects.ParserTreeObject(treeData);
+
+		for _,entry :=range entries{
+			fullPath :=filepath.Join(currentPath,entry.FileName);
+
+			if(entry.Type == "blob"){
+				blobData,err :=storage.ReadObject(entry.Hash);
+				if(err!=nil){
+					return err;
+				}
+				content :=objects.ExtractBlobContent(blobData);
+				
+				err =os.WriteFile(fullPath,content,0644);
+
+				if(err!=nil){
+					return err;
+				}
+
+
+
+			}else if(entry.Type=="tree"){
+				err :=os.MkdirAll(fullPath,0755);
+				if(err!=nil){
+					return err;
+				}
+				err =RestoreTree(entry.Hash,fullPath);
+				if(err!=nil){
+					return err;
+				}	
+				
+			}
+
+			}
+			return nil;
+			
+}
